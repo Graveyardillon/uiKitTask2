7f688d0349358fe9810522d9a670bdaa9b5f93da
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2019-11-04 03:17:05 +0900

    drawerの書き方

---
+++ uiKitTask2/uiKitTask2/Drawer.swift
@@ -0,0 +1,51 @@
+//
+//  Drawer.swift
+//  uiKitTask2
+//
+//  Created by Papillon on 2019/11/04.
+//  Copyright © 2019 Papillon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class Drawer: UIView, UITextFieldDelegate {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+  
+  override func draw(_ rect: CGRect) {
+    let bPath : UIBezierPath = UIBezierPath(rect: rect)
+    let fillColor : UIColor = UIColor.init(red: 255/255, green: 192/255, blue: 203/255, alpha: 1)
+    let strokeColor : UIColor = UIColor.init(red: 1, green: 0.5686, blue: 0.8314, alpha: 1)
+    
+    bPath.lineWidth = 3
+    fillColor.setFill()
+    bPath.fill()
+    strokeColor.setStroke()
+    strokeColor.set()
+    bPath.stroke()
+    
+    let myTextField: UITextField = UITextField()
+    myTextField.frame = CGRect(
+      origin: CGPoint(
+        x: rect.origin.x,
+        y: rect.origin.y + CGFloat(40)
+      ),
+      size: CGSize(
+        width: rect.size.width,
+        height: CGFloat(20)
+      )
+    )
+    
+    myTextField.placeholder = "Text..."
+    
+    myTextField.delegate = self
+    
+  }
+}
---
+++ uiKitTask2/uiKitTask2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uiKitTask2
+//
+//  Created by Papillon on 2019/11/04.
+//  Copyright © 2019 Papillon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
